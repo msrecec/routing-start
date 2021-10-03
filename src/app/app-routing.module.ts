c139fc98c57ab15afe4d49529e2f37a02c550c6a
msrecec <67606889+msrecec@users.noreply.github.com>
2021-10-03 13:22:25 +0200

    added resolver for async data

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,10 +6,10 @@ import { UserComponent } from "./users/user/user.component";
 import { EditServerComponent } from "./servers/edit-server/edit-server.component";
 import { ServerComponent } from "./servers/server/server.component";
 import { Routes, RouterModule } from "@angular/router";
-import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 import { AuthGuard } from "./auth-guard.service";
 import { CanDeactivateGuard } from "./servers/edit-server/can-deactivate-guard.service";
 import { ErrorPageComponent } from "./error-page/error-page.component";
+import { ServerResolver } from "./servers/server/server-reslover.service";
 /**
  * All routes for the application
  *
@@ -29,7 +29,11 @@ const appRoutes: Routes = [
     canActivateChild: [AuthGuard],
     component: ServersComponent,
     children: [
-      { path: ":id", component: ServerComponent },
+      {
+        path: ":id",
+        component: ServerComponent,
+        resolve: { server: ServerResolver },
+      },
       {
         path: ":id/edit",
         component: EditServerComponent,
